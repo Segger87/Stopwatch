ae53a45fbf1a8861c6df9409a3bc347abcf17fac
Segger87 <Segger87@gmail.com>
2018-07-11 15:40:08 +0100

    Simple Console App Stopwatch

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Stopwatch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new Program();
+			p.Execute();
+		}
+
+		private void Execute()
+		{
+			Console.WriteLine("Please type start to begin the stop watch and stop when you are finished.");
+			var initSw = Console.ReadLine().ToLower();
+			var sw = new Stopwatch();
+
+			switch (initSw)
+			{
+				case "start":
+					sw.Start();
+					Console.WriteLine("Timer has started");
+					break;
+				default:
+					Console.WriteLine("Sorry that is not a recognised command");
+					Execute();
+					break;
+			}
+
+
+			if (sw.Enabled)
+			{
+				Console.WriteLine("Please type stop when you are finished.");
+				var stopSw = Console.ReadLine().ToLower();
+
+				switch (stopSw)
+				{
+					case "stop":
+						sw.Stop();
+						var elapsedTime = sw.TimeElapsed;
+						Console.WriteLine($"The total time elapsed is {elapsedTime}");
+						Execute();
+						break;
+					default:
+						Console.WriteLine("Sorry that is not a recognised command");
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ Stopwatch/Stopwatch/Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stopwatch
+{
+	class Stopwatch
+	{
+		public bool Enabled;
+		public DateTime StartTime;
+		public DateTime EndTime;
+
+		public void Start()
+		{
+			StartTime = DateTime.Now;
+			Enabled = true;
+		}
+
+		public void Stop()
+		{
+			EndTime = DateTime.Now;
+			Enabled = false;
+		}
+
+		public TimeSpan TimeElapsed
+		{
+			get
+			{
+				var totalTime = StartTime - EndTime;
+				return totalTime;
+			}
+		}
+	}
+}
